24fdebdd4ed3443bcbd77ab9980bbb79d6cfe743
Sharon Jebitok <sharonkosgei4@gmail.com>
2022-06-16 23:31:55 +0300

    Add Changes to Hardhat Config File

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,27 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+const dotenv = require("dotenv");
+dotenv.config({path: __dirname + '/.env'});
+const { RPC_URL, PRIVATE_KEY } = process.env;
+require("@nomiclabs/hardhat-ethers")
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//   const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+//   for (const account of accounts) {
+//     console.log(account.address);
+//   }
+// });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    mumbai: {
+      url: RPC_URL || "",
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY,
+  }
 };
